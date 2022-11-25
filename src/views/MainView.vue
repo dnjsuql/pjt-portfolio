54a4e8ff517526a52b4bf6316e47d740ea4da66f
dnjsuql <68069455+dnjsuql@users.noreply.github.com>
2022-11-25 12:06:42 +0900

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="main">
-    <h1>퍼블리셔 / 프론트엔드 임원엽입니다.</h1>
-    <router-link to="/home">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="main__content">
+      <div class="main__title">
+        <h1>프로젝트 포트폴리오</h1>
+      </div>
+      <div class="main__link">
+        <router-link to="/home">웹퍼블리셔 임원엽입니다.</router-link> |
+        <router-link to="/about">웹디자이너 이영우입니다.</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,5 +28,25 @@ export default {
   width: 100%;
   height: 100vh;
   background-color: skyblue;
+  &__content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  &__title {
+    text-align: center;
+    h1 {
+      font-size: 48px;
+      font-weight: 700;
+      color: #fff;
+    }
+  }
+  &__link {
+    text-align: center;
+    font-size: 16px;
+    color: #fff;
+    font-weight: 500;
+  }
 }
 </style>
